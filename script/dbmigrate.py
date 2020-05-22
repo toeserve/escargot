c06d83d401e96513be0a03e90a5c139e0159a781
walkingphasers <helloimtheguy312@hotmail.com>
2020-05-22 13:37:30 -0400

    update DB schema

--- script/dbmigrate.py
+++ script/dbmigrate.py
@@ -1,4 +1,6 @@
 import db
 
 with db.Session() as sess:
-	sess.execute('ALTER TABLE t_user ADD COLUMN password_md5 TEXT DEFAULT \'\'')
+	sess.execute('DELETE FROM t_user WHERE t_user.email NOT IN (SELECT email FROM t_user GROUP BY LOWER(email))')
+	sess.execute('CREATE UNIQUE INDEX email_ci_index ON t_user (LOWER(email))')
+	sess.execute('DROP TABLE t_sound')
